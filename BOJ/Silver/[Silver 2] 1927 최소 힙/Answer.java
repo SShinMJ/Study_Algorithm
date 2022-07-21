09b0a3879bfde2d8bce8d4a3d56fee47add68c3f
SShinMJ <82142527+SShinMJ@users.noreply.github.com>
2022-07-21 11:37:05 +0900

    Add: BOJ-1927 Answer

---
+++ BOJ/Silver/[Silver 2] 1927 최소 힙/Answer.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        MinHeap mh = new MinHeap();
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                System.out.println(mh.delete());
+            } else {
+                mh.insert(input);
+            }
+        }
+    }
+}
+
+class MinHeap {
+    List<Integer> list;
+
+    public MinHeap() {
+        list = new ArrayList<>();
+        list.add(0);
+    }
+
+    public void insert(int val) {
+        list.add(val);
+        int current = list.size() - 1;
+        int parent = current / 2;
+        while (true) {
+            if (parent == 0 || list.get(parent) <= list.get(current)) {
+                break;
+            }
+
+            int temp = list.get(parent);
+            list.set(parent, list.get(current));
+            list.set(current, temp);
+
+            current = parent;
+            parent = current / 2;
+        }
+    }
+
+    public int delete() {
+        if (list.size() == 1) {
+            return 0;
+        }
+        int top = list.get(1);
+        list.set(1, list.get(list.size() - 1));
+        list.remove(list.size() - 1);
+
+        int currentPos = 1;
+        while (true) {
+            int leftPos = currentPos * 2;
+            int rightPos = currentPos * 2 + 1;
+
+            if (leftPos >= list.size()) {
+                break;
+            }
+
+            int minValue = list.get(leftPos);
+            int minPos = leftPos;
+
+            if (rightPos < list.size() && minValue > list.get(rightPos)) {
+                minValue = list.get(rightPos);
+                minPos = rightPos;
+            }
+
+            if(list.get(currentPos) > minValue) {
+                int temp = list.get(currentPos);
+                list.set(currentPos, minValue);
+                list.set(minPos, temp);
+                currentPos = minPos;
+            } else {
+                break;
+            }
+        }
+        return top;
+    }
+}
