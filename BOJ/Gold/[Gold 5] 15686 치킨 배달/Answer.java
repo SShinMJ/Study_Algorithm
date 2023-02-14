1f6aa59da329be0a9fdefbd807e3322516590598
SShinMJ <winny0678@gmail.com>
2023-02-14 14:43:29 +0900

    Add: BOJ-15686

---
+++ BOJ/Gold/[Gold 5] 15686 치킨 배달/Answer.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// DFS 백트래킹
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static ArrayList<Point> person;
+    static ArrayList<Point> chicken;
+    static int ans;
+    static boolean[] open;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        person = new ArrayList<>();
+        chicken = new ArrayList<>();
+
+        // 미리 집과 치킨집에 해당하는 좌표를 ArrayList에 넣어 둠.
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if (map[i][j] == 1) {
+                    person.add(new Point(i, j));
+                } else if (map[i][j] == 2) {
+                    chicken.add(new Point(i, j));
+                }
+            }
+        }
+
+        ans = Integer.MAX_VALUE;
+        open = new boolean[chicken.size()];
+
+        DFS(0, 0);
+
+        System.out.println(ans);
+    }
+
+    public static void DFS(int start, int cnt) {
+        // 아래 for문을 통해 치킨집 좌표리스트에서 M개가 open가 되었다면,
+        if (cnt == M) {
+            int res = 0;
+
+            for (int i = 0; i < person.size(); i++) {
+                int temp = Integer.MAX_VALUE;
+
+                // 어떤 집과 치킨집 중 open한 치킨집의 모든 거리를 비교한다.
+                // 그 중, 최소 거리를 구한다.
+                for (int j = 0; j < chicken.size(); j++) {
+                    if (open[j]) {
+                        int distance = Math.abs(person.get(i).x - chicken.get(j).x)
+                                + Math.abs(person.get(i).y - chicken.get(j).y);
+
+                        temp = Math.min(temp, distance);
+                    }
+                }
+                res += temp;
+            }
+            ans = Math.min(ans, res);
+            return;
+        }
+
+        // 백트래킹
+        // start를 매개변수로 사용해서 가지치기
+        for (int i = start; i < chicken.size(); i++) {
+            open[i] = true;
+            DFS(i + 1, cnt + 1);
+            open[i] = false;
+        }
+    }
+}
