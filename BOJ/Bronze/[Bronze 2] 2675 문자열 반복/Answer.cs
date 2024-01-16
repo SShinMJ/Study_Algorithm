643e5451c37328bb5ed1fd65fee65185b8725fa1
SShinMJ <winny0678@gmail.com>
2024-01-16 11:27:19 +0900

    Add: BOJ-2675

---
+++ BOJ/Bronze/[Bronze 2] 2675 문자열 반복/Answer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Text;
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            int testCount = int.Parse(Console.ReadLine());
+            StringBuilder output = new StringBuilder();
+ 
+            for (int i = 0; i < testCount; i++)
+            {
+                string[] testCases = Console.ReadLine().Split(' ');
+                int spellRepeatCount = int.Parse(testCases[0]);
+                string testString = testCases[1];
+ 
+                for (int j = 0; j < testString.Length; j++)
+                {
+                    output.Append(testString[j], spellRepeatCount);
+                }
+ 
+                output.Append("\n");
+            }
+ 
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
