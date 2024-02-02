0868d06e2e59b362484a0289e656f8b092e0a485
SShinMJ <winny0678@gmail.com>
2024-02-02 11:44:22 +0900

    Add: BOJ-10818

---
+++ BOJ/Bronze/ [Bronze 3] 10818번 최소, 최대/Answer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            string[] s = Console.ReadLine().Split();
+            
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(s[i]);
+            }
+            
+            Array.Sort(arr);
+            
+            Console.WriteLine(arr[0] + " " + arr[n - 1]);
+        }
+    }
+}
