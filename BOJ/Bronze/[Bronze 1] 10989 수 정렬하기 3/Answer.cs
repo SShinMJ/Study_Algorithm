d5610c38ff1a5e646f86a7d6d15a84afc4ed2a56
SShinMJ <winny0678@gmail.com>
2024-03-02 12:14:56 +0900

    Add: BOJ-10989

---
+++ BOJ/Bronze/[Bronze 1] 10989 수 정렬하기 3/Answer.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace BOJ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = new StreamWriter(Console.OpenStandardOutput());
+            StreamReader reader = new StreamReader(Console.OpenStandardInput());
+            StringBuilder sb = new StringBuilder();
+
+            int count = int.Parse(reader.ReadLine());
+            int[] list = new int[10001];
+
+            for (int i = 0; i < count; i++)
+            {
+                list[int.Parse(reader.ReadLine())]++;
+            }
+            for (int i = 0; i <= 10000; i++)
+            {
+                if (list[i] != 0)
+                {
+                    for (int j = 0; j < list[i]; j++)
+                    {
+                        sb.Append(i + "\n");
+                    }
+                }
+            }
+
+            writer.WriteLine(sb);
+
+            writer.Close();
+            reader.Close();
+        }
+    }
+}
